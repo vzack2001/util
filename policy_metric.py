016100ce267eebbb984d3bc8c92e10e227f14032
vzack2001 <vzack2001@gmail.com>
2022-02-24 14:42:38 +0700

    add overtime param

--- policy_metric.py
+++ policy_metric.py
@@ -15,12 +15,15 @@ class PolicyStat(keras.metrics.Metric):
         self.action_pos = self.add_weight(name='action_pos', shape=weight_shape, initializer='zeros')
         self.action_neg = self.add_weight(name='action_neg', shape=weight_shape, initializer='zeros')
         self.action_sum = self.add_weight(name='action_sum', shape=weight_shape, initializer='zeros')
+        self.overtime = self.add_weight(name='overtime', shape=weight_shape, initializer='zeros')
 
         pass  # __init__()
 
     def update_state(self, y_true, log_probs, sample_weight=None):
 
-        act_rewards = y_true[:,:3]               # (?,3)
+        act_rewards = y_true[:,:3]               # (?,3) ['none', 'buy', 'sell',]
+        done_time = y_true[:,3:6]                # (?,3) ['done', 'buy_time_100', 'sell_time_100',]
+
         #log_probs, values, features = model(x)  # (?,3)
         #print_ndarray('act_rewards, log_prob', np.concatenate([act_rewards, log_probs], axis=-1), 16, frm='8.3f')
 
@@ -39,35 +42,39 @@ class PolicyStat(keras.metrics.Metric):
         #  tf.math.greater(a, 0.0)
         action_pos_mask = tf.where(act_rewards > 0, 1.0, 0.0)   # (?,3)
         action_neg_mask = tf.where(act_rewards < 0, 1.0, 0.0)   # (?,3)
-        #print_ndarray('act_rewards, log_probs', np.concatenate([act_rewards, policy_actions, policy_actions_act, action_pos_mask, action_neg_mask], axis=-1), 16, frm='5.0f')
+        overtime = tf.where(done_time * policy_actions_act > 7200, 1.0, 0.0)          # (?,3)
 
         per_action_pos = tf.reduce_sum(action_pos_mask * policy_actions_act, axis=0)  # (3,)
         per_action_neg = tf.reduce_sum(action_neg_mask * policy_actions_act, axis=0)  # (3,)
         action_pos = tf.reduce_sum(action_pos_mask, axis=0)     # (3,)
         action_neg = tf.reduce_sum(action_neg_mask, axis=0)     # (3,)
         action_sum = tf.reduce_sum(policy_actions_act, axis=0)  # (3,)
+        overtime = tf.reduce_sum(overtime, axis=0)              # (3,)
 
         self.per_action_pos.assign_add(per_action_pos)
         self.per_action_neg.assign_add(per_action_neg)
         self.action_pos.assign_add(action_pos)
         self.action_neg.assign_add(action_neg)
         self.action_sum.assign_add(action_sum)
+        self.overtime.assign_add(overtime)
 
         pass  # update_state()
 
     def __str__(self):
-        action_sum, per_action_pos, per_action_neg = self.result().numpy()
+        action_sum, per_action_pos, per_action_neg, overtime = self.result().numpy()
         per_action_pos = '[' + ''.join(['{:5.1f}'.format(s) for s in per_action_pos[1:]]) + ']'
         per_action_neg = '[' + ''.join(['{:5.1f}'.format(s) for s in per_action_neg]) + ']'
         action_sum = '[' + ''.join(['{:5.1f}'.format(s) for s in action_sum]) + ']'
-        return f'{action_sum} {per_action_pos} {per_action_neg}'
+        overtime = '[' + ''.join(['{:5.1f}'.format(s) for s in overtime[1:]]) + ']'
+        return f'{action_sum} {per_action_pos} {per_action_neg} {overtime}'
 
     def result(self):
         epsilon = 1e-6
         action_sum = self.action_sum / tf.reduce_sum(self.action_sum + epsilon) * 100.
         per_action_pos = self.per_action_pos/(self.action_pos + epsilon) * 100.
         per_action_neg = self.per_action_neg/(self.action_sum + epsilon) * 100.
-        return (action_sum, per_action_pos, per_action_neg)
+        overtime = self.overtime/(self.action_sum + epsilon) * 100.
+        return (action_sum, per_action_pos, per_action_neg, overtime)
 
     def reset_state(self):
         backend.batch_set_value([(v, np.zeros(v.shape)) for v in self.variables])
